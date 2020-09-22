1d68edc3d427e6443b326e051d1241d9d0aa0f65
Sinyoung3016 <tasha0415@naver.com>
2020-09-22 20:58:06 +0900

    Update OOP week04 lab

---
+++ OOP_C++/Project4/Project4/practice01.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main() {
+
+	char string[101];
+	printf("문자를 입력하세요.(100자 이내)\n");
+	scanf("%s", string);
+	printf("입력된 문자는 %s입니다.", string);
+
+	return 0;
+}
---
+++ OOP_C++/Project4/Project4/reference.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+void increment(int& x) {
+	++x;
+}
+
+int main() {
+	using namespace std;
+
+	int x = 10;
+	
+	cout << "증가 이전 값 :" << x << endl;
+	increment(x);
+	cout << "증가 이후 값 :" << x << endl;
+
+	system("pause");
+	return 0;
+}
