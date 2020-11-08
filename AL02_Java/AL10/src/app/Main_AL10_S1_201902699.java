3dd2c8f9d169a9dbca6099d132504a9d48f86f0c
Sinyoung3016 <tasha0415@naver.com>
2020-11-08 21:21:29 +0900

    Update AL02 week10 : Sort

---
+++ AL02_Java/AL10/src/app/Main_AL10_S1_201902699.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Main_AL10_S1_201902699 {
+    public static void main(String[] args) {
+        AppController appController = new AppController() ;
+        appController.run() ;
+    }
+}
